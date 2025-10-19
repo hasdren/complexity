5b6094433b9be278d3d27d042c536090b71db15b
karolis <seibutis.karolis@gmail.com>
2025-10-19 23:06:32 +0300

    case 23

--- complexity2.js
+++ complexity2.js
@@ -2,9 +2,10 @@ app.post("/log-nutrients", async (req, res) => {
   const { username, logDate, calories, protein, fats, carbohydrates, water } =
     req.body;
 
-  if (!username || !calories || !protein || !fats || !carbohydrates || !water) {
+  /*if (!username || !calories || !protein || !fats || !carbohydrates || !water) {
     return res.status(400).json({ error: "All nutrient fields are required." });
   }
+  /*
 
   /*const activityDate = logDate ? new Date(logDate) : new Date();
    */
