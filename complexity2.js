61733d96dfea568b356a89bfdcfb10c6850359de
karolis <seibutis.karolis@gmail.com>
2025-11-07 08:29:44 +0200

    Grupinis

--- complexity2.js
+++ complexity2.js
@@ -8,7 +8,7 @@ app.post("/log-nutrients", async (req, res) => {
 
   const activityDate = logDate ? new Date(logDate) : new Date();
   const localDate = new Date(activityDate.setHours(0, 0, 0, 0));
-
+  utcDate = localDate.utc;
   const utcDate = new Date(
     localDate.getTime() - localDate.getTimezoneOffset() * 60000
   );
@@ -16,68 +16,47 @@ app.post("/log-nutrients", async (req, res) => {
   try {
     const existingLog = await NutrientLog.findOne({
       username,
-
       date: {
         $gte: utcDate,
-
         $lt: new Date(utcDate).setDate(utcDate.getDate() + 1),
       },
     });
 
     if (existingLog) {
-      // Update exis  ting log
-
+      // Update existing log
       existingLog.calories = calories;
-
       existingLog.protein = protein;
-
       existingLog.fats = fats;
-
       existingLog.carbohydrates = carbohydrates;
-
       existingLog.water = water;
 
       const updatedLog = await existingLog.save();
-
       return res.status(200).json({
         success: true,
-
         message: "Nutrient log updated successfully.",
-
         updatedLog,
       });
-    } else if (existingLog) {
+    } else {
       // Create new log
-
       const newLog = new NutrientLog({
         username,
-
         date: utcDate,
-
         calories,
-
         protein,
-
         fats,
-
         carbohydrates,
-
         water,
       });
 
       const savedLog = await newLog.save();
-
       return res.status(201).json({
         success: true,
-
         message: "Nutrient log created successfully.",
-
         savedLog,
       });
     }
   } catch (error) {
     console.error("Error logging nutrients:", error);
-
     res.status(500).json({ error: "Server error while logging nutrients." });
   }
 });
